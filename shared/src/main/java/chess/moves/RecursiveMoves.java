215bd632ba46ee1c82d657c495dea8094535c0cc
Zach Thompson <zach@zach-thompson.com>
2024-06-26 13:09:06 -0600

    Finished Bishop move tests

--- shared/src/main/java/chess/moves/RecursiveMoves.java
+++ shared/src/main/java/chess/moves/RecursiveMoves.java
@@ -21,20 +21,25 @@ public class RecursiveMoves {
      * @param type The piece type, to dictate what moves to calculate
      * @return An array of all possible moves for the given piece
      */
-    public Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition myPosition, ChessPiece.PieceType type) {
+    public Collection<ChessMove> getValidMoves(
+            ChessBoard board,
+            ChessPosition myPosition,
+            ChessPiece.PieceType type
+    ) {
 
         ArrayList<ChessMove> validMoves = new ArrayList<>();
 
         // Two if statements should allow the queen to hit both horizontal and diagonal moves
         if (type == ChessPiece.PieceType.ROOK || type == ChessPiece.PieceType.QUEEN) {
             for (int[] move : possibleStraightMoves) {
-                ArrayList<ChessMove> directionMoves = recursiveFindAllMoves(myPosition, move);
+                ArrayList<ChessMove> directionMoves = recursiveFindAllMoves(board, myPosition, null, move);
             }
         }
 
         if (type == ChessPiece.PieceType.BISHOP || type == ChessPiece.PieceType.QUEEN) {
             for (int[] move : possibleDiagonalMoves) {
-                ArrayList<ChessMove> directionMoves = recursiveFindAllMoves(myPosition, move);
+
+                validMoves.addAll(recursiveFindAllMoves(board, myPosition, null, move));
             }
         }
 
@@ -48,36 +53,55 @@ public class RecursiveMoves {
      * @param currentMove The direction to continue heading
      * @return An Array of all the moves in a single direction
      */
-    private ArrayList<ChessMove> recursiveFindAllMoves(ChessPosition myPosition, int[] currentMove) {
+    private ArrayList<ChessMove> recursiveFindAllMoves(
+            ChessBoard board,
+            ChessPosition myPosition,
+            ChessPosition newPosition,
+            int[] currentMove
+    ) {
+
         ArrayList<ChessMove> allMoves = new ArrayList<ChessMove>();
 
-        boolean pathing = true;
+        if (newPosition == null) {
+            newPosition = myPosition;
+        }
+        // Get current location
+        int currRow = newPosition.getRow() - 1;
+        int currCol = newPosition.getColumn() - 1;
 
-        while (pathing) {
+        // Get new location
+        int newRow = currRow + currentMove[0];
+        int newColumn = currCol + currentMove[1];
 
-            // Get current location
-            int currRow = myPosition.getRow();
-            int currCol = myPosition.getColumn();
 
-            // Get new location
-            int newRow = currRow + currentMove[0];
-            int newColumn = currCol + currentMove[1];
-
-            // Check for out of bounds
-            if (newRow > 7 || newColumn > 7 || newRow < 0 || newColumn < 0) {
-                pathing = false;
-                break;
-            }
-
+        // Check for out of bounds
+        if (newRow > 7 || newColumn > 7 || newRow < 0 || newColumn < 0) {
+            return allMoves;
+        }
+        else {
             // Create the new position and
-            ChessPosition newPosition = new ChessPosition(newRow, newColumn);
+            newPosition = new ChessPosition(newRow + 1, newColumn + 1);
             ChessMove validMove = new ChessMove(myPosition, newPosition, null);
 
-            allMoves.add(validMove);
+            // Check for collision with another friendly piece
+            if (board.getPiece(newPosition) != null) {
+                // If its friendly, dont add the space
+                if (board.getPiece(newPosition).getTeamColor() == board.getPiece(myPosition).getTeamColor()) {
+                    return allMoves;
+                }
+                // If its enemy, add the space but stop path finding.
+                else {
+                    allMoves.add(validMove);
+                    return allMoves;
+                }
+
+            }
 
             // Loop again
-            allMoves.addAll(recursiveFindAllMoves(newPosition, currentMove));
+            allMoves.addAll(recursiveFindAllMoves(board, myPosition, newPosition, currentMove));
+            allMoves.add(validMove);
         }
+
 
         return allMoves;
     }

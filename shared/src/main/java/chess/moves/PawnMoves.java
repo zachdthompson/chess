83d5ef2601f20077de0da542a16dba7d6b23feea
Zach Thompson <zach@zach-thompson.com>
2024-06-26 15:07:55 -0600

    Changed order of out of bounds checking

--- shared/src/main/java/chess/moves/PawnMoves.java
+++ shared/src/main/java/chess/moves/PawnMoves.java
@@ -50,32 +50,39 @@ public class PawnMoves {
                 break;
         }
 
+
         // Check if we can capture
         ChessPosition leftPosition = new ChessPosition(myPosition.getRow() + verticalDirection, myPosition.getColumn() - 1);
         ChessPosition rightPosition = new ChessPosition(myPosition.getRow() + verticalDirection, myPosition.getColumn() + 1);
 
-        if (diagonalAttackDetection(board, leftPosition, piece.getTeamColor())) {
-            // Check for promotions
-            if (leftPosition.getRow() == 8 || leftPosition.getRow() == 1 ) {
-                moves.add(new ChessMove(myPosition, leftPosition, ChessPiece.PieceType.QUEEN));
-                moves.add(new ChessMove(myPosition, leftPosition, ChessPiece.PieceType.BISHOP));
-                moves.add(new ChessMove(myPosition, leftPosition, ChessPiece.PieceType.KNIGHT));
-                moves.add(new ChessMove(myPosition, leftPosition, ChessPiece.PieceType.ROOK));
-            }
-            else {
-                moves.add(new ChessMove(myPosition, leftPosition, null));
+        // Check if the move is out of bounds first
+        if (!isOutOfBounds(leftPosition)) {
+
+            if (diagonalAttackDetection(board, leftPosition, piece.getTeamColor())) {
+                // Check for promotions
+                if (leftPosition.getRow() == 8 || leftPosition.getRow() == 1) {
+                    moves.add(new ChessMove(myPosition, leftPosition, ChessPiece.PieceType.QUEEN));
+                    moves.add(new ChessMove(myPosition, leftPosition, ChessPiece.PieceType.BISHOP));
+                    moves.add(new ChessMove(myPosition, leftPosition, ChessPiece.PieceType.KNIGHT));
+                    moves.add(new ChessMove(myPosition, leftPosition, ChessPiece.PieceType.ROOK));
+                } else {
+                    moves.add(new ChessMove(myPosition, leftPosition, null));
+                }
             }
         }
-        if (diagonalAttackDetection(board, rightPosition, piece.getTeamColor())) {
-            // Check for promotions
-            if (rightPosition.getRow() == 8 || rightPosition.getRow() == 1 ) {
-                moves.add(new ChessMove(myPosition, rightPosition, ChessPiece.PieceType.QUEEN));
-                moves.add(new ChessMove(myPosition, rightPosition, ChessPiece.PieceType.BISHOP));
-                moves.add(new ChessMove(myPosition, rightPosition, ChessPiece.PieceType.KNIGHT));
-                moves.add(new ChessMove(myPosition, rightPosition, ChessPiece.PieceType.ROOK));
-            }
-            else {
-                moves.add(new ChessMove(myPosition, rightPosition, null));
+
+        // Check if out of bounds
+        if (!isOutOfBounds(rightPosition)) {
+            if (diagonalAttackDetection(board, rightPosition, piece.getTeamColor())) {
+                // Check for promotions
+                if (rightPosition.getRow() == 8 || rightPosition.getRow() == 1) {
+                    moves.add(new ChessMove(myPosition, rightPosition, ChessPiece.PieceType.QUEEN));
+                    moves.add(new ChessMove(myPosition, rightPosition, ChessPiece.PieceType.BISHOP));
+                    moves.add(new ChessMove(myPosition, rightPosition, ChessPiece.PieceType.KNIGHT));
+                    moves.add(new ChessMove(myPosition, rightPosition, ChessPiece.PieceType.ROOK));
+                } else {
+                    moves.add(new ChessMove(myPosition, rightPosition, null));
+                }
             }
         }
 
@@ -86,25 +93,29 @@ public class PawnMoves {
         // If we can move forward, add the move
         if (!verticalCollisionDetection(board, newPosition)) {
 
-            // Check if we can move twice on the first move
-            if (myPosition.getRow() == startRow) {
-                ChessPosition secondMove = new ChessPosition(newPosition.getRow() + verticalDirection, newPosition.getColumn());
+           if (!isOutOfBounds(newPosition)) {
 
-                if (!verticalCollisionDetection(board, secondMove)) {
-                    moves.add(new ChessMove(myPosition, secondMove, null));
-                }
-            }
+               // Check if we can move twice on the first move
+               if (myPosition.getRow() == startRow) {
+                   ChessPosition secondMove = new ChessPosition(newPosition.getRow() + verticalDirection, newPosition.getColumn());
+                   // Check if the move is out of bounds
+                   if (!isOutOfBounds(secondMove)) {
+                       if (!verticalCollisionDetection(board, secondMove)) {
+                           moves.add(new ChessMove(myPosition, secondMove, null));
+                       }
+                   }
+               }
 
-            // Check for promotions
-            if (newPosition.getRow() == 8 || newPosition.getRow() == 1 ) {
-                moves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.QUEEN));
-                moves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.BISHOP));
-                moves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.KNIGHT));
-                moves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.ROOK));
-            }
-            else {
-                moves.add(new ChessMove(myPosition, newPosition, null));
-            }
+               // Check for promotions
+               if (newPosition.getRow() == 8 || newPosition.getRow() == 1) {
+                   moves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.QUEEN));
+                   moves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.BISHOP));
+                   moves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+                   moves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.ROOK));
+               } else {
+                   moves.add(new ChessMove(myPosition, newPosition, null));
+               }
+           }
 
         }
 
@@ -125,6 +136,18 @@ public class PawnMoves {
         }
         return false;
     }
+
+
+    private boolean isOutOfBounds(ChessPosition checkPosition) {
+        int row = checkPosition.getRow();
+        int column = checkPosition.getColumn();
+
+        if (row < 1 || row > 8 || column < 1 || column > 8) {
+            return true;
+        }
+        return false;
+    }
+
 
 
 }

928883f5ecba1fc0b25b211ee034eb7002f5ee40
Zach Thompson <zach@zach-thompson.com>
2024-07-10 10:40:33 -0600

    Most things working, still working on check/checkmate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,5 +1,7 @@
 package chess;
 
+import chess.moves.KingMoves;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
@@ -37,19 +39,6 @@ public class ChessGame {
      */
     public void setTeamTurn(TeamColor team) {
         teamTurn = team;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ChessGame chessGame = (ChessGame) o;
-        return teamTurn == chessGame.teamTurn && Objects.equals(chessBoard, chessGame.chessBoard);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(teamTurn, chessBoard);
     }
 
     /**
@@ -92,8 +81,16 @@ public class ChessGame {
 
         ArrayList<ChessMove> possibleMoves = new ArrayList<>(validMoves(move.getStartPosition()));
 
+        // Check if the desired move is valid
         if (!possibleMoves.contains(move)) {
             throw new InvalidMoveException("Invalid move: " + move);
+        }
+
+        //If it' s the king, make sure the move won't put them in check
+        if (chessBoard.getPiece(move.getStartPosition()).getPieceType() == ChessPiece.PieceType.KING) {
+            if (moveWillEndangerKing(move)) {
+                throw new InvalidMoveException("That move puts your king in check!");
+            }
         }
 
         // Do move here
@@ -116,7 +113,27 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+
+        // Set opposing team color
+        TeamColor opposingTeam = TeamColor.WHITE;
+
+        if (teamColor == TeamColor.WHITE) {
+            opposingTeam = TeamColor.BLACK;
+        }
+
+        // Get all their remaining pieces
+        Collection<ChessPiece> opposingPieces = getTeamPieces(opposingTeam);
+
+        // TODO: Get a list of all the opposite team valid moves
+        ArrayList<ChessMove> validOpponentMoves = new ArrayList<>();
+
+        for (ChessPiece opposingPiece : opposingPieces) {
+            ChessPosition currentPiecePosition =
+            validOpponentMoves.addAll(opposingPiece.pieceMoves(chessBoard, ))
+        }
+        // TODO: Check if those moves correspond to our king
+
+        return false;
     }
 
     /**
@@ -126,7 +143,12 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+
+        // TODO: Check if king is currently in check
+        // TODO: Get list of all king moves
+        // TODO: See if any valid moves are left that wouldn't put them in check
+
+        return false;
     }
 
     /**
@@ -137,7 +159,11 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+
+        // TODO: Get list of all valid moves for that team
+
+
+        return false;
     }
 
     /**
@@ -156,5 +182,49 @@ public class ChessGame {
      */
     public ChessBoard getBoard() {
         return chessBoard;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessGame chessGame = (ChessGame) o;
+        return teamTurn == chessGame.teamTurn && Objects.equals(chessBoard, chessGame.chessBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamTurn, chessBoard);
+    }
+
+    private boolean moveWillEndangerKing(ChessMove move) {
+
+        // Make a local copy of the board
+        ChessBoard copyBoard = chessBoard.clone();
+
+        // Make the move on the copy and check if it puts the king in check
+        copyBoard.movePiece(move);
+
+        return isInCheckmate(teamTurn);
+    }
+
+    /**
+     * Gets all the pieces for the given team
+     * @param teamColor The team color you want all the pieces for
+     * @return ArrayList of pieces
+     */
+    private Collection<ChessPiece> getTeamPieces(TeamColor teamColor) {
+        ArrayList<ChessPiece> pieces = new ArrayList<>();
+
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                ChessPiece piece = chessBoard.getPiece(new ChessPosition(row, col));
+                if (piece.getTeamColor() == teamColor) {
+                    pieces.add(piece);
+                }
+            }
+        }
+
+        return pieces;
     }
 }

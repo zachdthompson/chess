b4b4c99094e18c816896a14fd46eb5670bb72a10
Zach Thompson <zach@zach-thompson.com>
2024-07-10 12:36:00 -0600

    Change knight moves to final

--- shared/src/main/java/chess/moves/KnightMoves.java
+++ shared/src/main/java/chess/moves/KnightMoves.java
@@ -9,17 +9,17 @@ import java.util.Collection;
 
 public class KnightMoves extends MoveGenerator {
 
-    private int[] UP_LEFT = {2, -1};
-    private int[] DOWN_LEFT = {-2, -1};
-    private int[] UP_RIGHT = {2, 1};
-    private int[] DOWN_RIGHT = {-2, 1};
+    private final int[] UP_LEFT = {2, -1};
+    private final int[] DOWN_LEFT = {-2, -1};
+    private final int[] UP_RIGHT = {2, 1};
+    private final int[] DOWN_RIGHT = {-2, 1};
 
-    private int[] LEFT_UP = {1, -2};
-    private int[] RIGHT_UP = {1, 2};
-    private int[] LEFT_DOWN = {-1, -2};
-    private int[] RIGHT_DOWN = {-1, 2};
+    private final int[] LEFT_UP = {1, -2};
+    private final int[] RIGHT_UP = {1, 2};
+    private final int[] LEFT_DOWN = {-1, -2};
+    private final int[] RIGHT_DOWN = {-1, 2};
 
-    private int[][] validKnightMoves = {UP_LEFT, DOWN_LEFT, UP_RIGHT, DOWN_RIGHT,
+    private final int[][] validKnightMoves = {UP_LEFT, DOWN_LEFT, UP_RIGHT, DOWN_RIGHT,
     LEFT_UP, RIGHT_UP, LEFT_DOWN, RIGHT_DOWN};
 
     @Override

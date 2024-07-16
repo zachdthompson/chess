fb582ba2cc101f9ff05569f7e7c314835aa90fdd
Zach Thompson <zach@zach-thompson.com>
2024-07-16 11:57:13 -0600

    Generated test cases for DAO

---
+++ server/test/java/dataaccess/dao/MemoryUserDAOTest.java
@@ -0,0 +1,31 @@
+package dataaccess.dao;
+
+import dataaccess.DataAccessException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryUserDAOTest {
+
+    @BeforeEach
+    void setUp() throws DataAccessException {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void clear() {
+    }
+}

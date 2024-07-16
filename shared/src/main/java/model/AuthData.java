1b125b25ebaf4f2c440c86cea9cf4361f1b08ba0
Zach Thompson <zach@zach-thompson.com>
2024-07-16 11:44:58 -0600

    Updated AuthData to securely generate token

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,3 +1,21 @@
 package model;
 
-public record AuthData(String username, String authToken) {}
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public record AuthData(String authToken, String username) {
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder().withoutPadding();
+
+    /**
+     * Randomly generates a secure auth token.
+     * This occurs by generating a random string of bytes, then pulling from that for the token
+     * @return String - Random identifier
+     */
+    public static String createToken() {
+        byte[] randomBytes = new byte[24];
+        secureRandom.nextBytes(randomBytes);
+        return base64Encoder.encodeToString(randomBytes);
+    }
+}

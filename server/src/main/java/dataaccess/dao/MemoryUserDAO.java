87e9b2f616be9e784c959da0959f322461b3aca6
Zach Thompson <zach@zach-thompson.com>
2024-07-16 13:21:37 -0600

    Added password hashing

--- server/src/main/java/dataaccess/dao/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/dao/MemoryUserDAO.java
@@ -2,6 +2,8 @@ package dataaccess.dao;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import model.UserData;
 
@@ -18,8 +20,14 @@ public class MemoryUserDAO {
      * @return UserData of the user
      */
     public UserData createUser(UserData user) {
-        users.put(user.username(), user);
-        return user;
+
+        // Store the user password as a SHA-512 hash
+        String hashedPassword = hashPassword(user.password());
+
+        UserData hashedUser = new UserData(user.username(),hashedPassword, user.email());
+
+        users.put(user.username(), hashedUser);
+        return hashedUser;
     }
 
     /**
@@ -36,5 +44,35 @@ public class MemoryUserDAO {
      */
     public void clear() {
         users.clear();
+    }
+
+    /**
+     * Hash the password in SHA-512 for secure storage
+     * This function was taken from ChatGPT
+     * @param password The password to hash
+     * @return String of SHA-512 hashed password
+     */
+    public static String hashPassword(String password) {
+        try {
+            // Create a MessageDigest instance for SHA-512
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+            // Add password bytes to digest
+            md.update(password.getBytes());
+
+            // Get the hash's bytes
+            byte[] bytes = md.digest();
+
+            // Convert byte array into signum representation
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02x", b));
+            }
+
+            // Return complete hashed password in hex format
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

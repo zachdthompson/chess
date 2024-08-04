8a4a00e64cef6db155aa1d39906fe6162eb0e4c7
Zach Thompson <zach@zach-thompson.com>
2024-08-04 14:48:45 -0600

    Added info to allow server to run on its own

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,14 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        // Start the server on a local port
+        int desiredPort = 8080;
+        Server server = new Server();
+        server.run(desiredPort);
     }
 }
